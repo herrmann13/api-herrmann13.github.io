ec46ee0e59fde707a327bba9771a7b0f46be4c97
Henrique Herrmann <h.herrmann@estudantes.ifg.edu.br>
2026-04-22 17:15:06 -0300

    feat: rota health mapeada, rota projects mapeada mas nao implementada

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"log"
+	"time"
 	"net/http"
-
 	"api-herrmann13.github.io/internal/system"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/health", system.HealthHandler)
+	startedAt := time.Now()
+	mux.HandleFunc("/health", system.HealthHandler(startedAt))
+	mux.HandleFunc("/projects", system.ProjectsHandler())
 
 	log.Println("Server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
